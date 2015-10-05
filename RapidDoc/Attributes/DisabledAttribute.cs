dfa8ee84a7f9b5b63d54f4b1f1c6339fe1614321
AltyntauSource <igDmitrov@gmail.com>
2015-10-05 17:10:58 +0600

    Add function disabled elements enum

---
+++ RapidDoc/Attributes/DisabledAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RapidDoc.Attributes
+{
+    public class DisabledAttribute : Attribute
+    {
+        public DisabledAttribute() : base() { }
+    }
+}
