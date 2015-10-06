54e6687796f9e72e1e56f054e33c6ea2413e6a3c
AltyntauSource <igDmitrov@gmail.com>
2015-10-06 17:11:50 +0600

    Html editor

--- RapidDoc/Scripts/main.js
+++ RapidDoc/Scripts/main.js
@@ -153,6 +153,60 @@ function summernote_init(lang) {
     }
 }
 
+function summernotemin_init(lang) {
+    if ((lang == "") || (lang == "")) {
+        lang = 'en-US';
+    }
+
+    if ($(".summernotemin")[0]) {
+        $('.summernotemin').summernote({
+            height: 100,
+            focus: false,
+            lang: lang,
+            defaultFontName: 'Arial',
+            toolbar: [],
+            onPaste: function (e) {
+                var thisNote = $(this);
+                var updatePastedText = function (someNote) {
+                    var original = someNote.code();
+                    var cleaned = cleanPastedHTML(original); //this is where to call whatever clean function you want. I have mine in a different file, called CleanPastedHTML.
+                    someNote.code(cleaned);
+                };
+
+                setTimeout(function () {
+                    updatePastedText(thisNote);
+                }, 10);
+            }
+        });
+    }
+}
+
+function cleanPastedHTML(input) {
+    // 1. remove line breaks / Mso classes
+    var stringStripper = /(\n|\r| class=(")?Mso[a-zA-Z]+(")?)/g;
+    var output = input.replace(stringStripper, ' ');
+    // 2. strip Word generated HTML comments
+    var commentSripper = new RegExp('<!--(.*?)-->', 'g');
+    var output = output.replace(commentSripper, '');
+    var tagStripper = new RegExp('<(/)*(meta|link|span|strong|h1|h2|h3|h4|h5|h6|small|\\?xml:|st1:|o:|font)(.*?)>', 'gi');
+    // 3. remove tags leave content if any
+    output = output.replace(tagStripper, '');
+    // 4. Remove everything in between and including tags '<style(.)style(.)>'
+    var badTags = ['style', 'script', 'applet', 'embed', 'noframes', 'noscript', 'img'];
+
+    for (var i = 0; i < badTags.length; i++) {
+        tagStripper = new RegExp('<' + badTags[i] + '.*?' + badTags[i] + '(.*?)>', 'gi');
+        output = output.replace(tagStripper, '');
+    }
+    // 5. remove attributes ' style="..."'
+    var badAttributes = ['style', 'start'];
+    for (var i = 0; i < badAttributes.length; i++) {
+        var attributeStripper = new RegExp(' ' + badAttributes[i] + '="(.*?)"', 'gi');
+        output = output.replace(attributeStripper, '');
+    }
+    return output;
+}
+
 function grid_init(url_json) {
     var self = this;
     this.currentPage = 1;
